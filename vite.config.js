81d4da9cbe9e3ee614625de8dda6c54a357fa868
Link1515 <terrylin1515@gmail.com>
2026-05-20 14:53:01 +0800

    build: add Vue DevTools to Vite setup

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   base: '/baplcp-signup/',
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./src', import.meta.url)),
