9f41ab148e5037f7fe69c41158f2de8d87855a80
Hedy <heydesign01@gmail.com>
2026-06-24 16:18:53 +0800

    手動上線 2026-06-24 16:18:53

--- src/components/home/HomeParticipationCard.vue
+++ src/components/home/HomeParticipationCard.vue
@@ -31,9 +31,13 @@ const iconBurn = import.meta.env.BASE_URL + 'images/icon-burn.png'
       <img :src="iconBurn" alt="" class="burn-icon" />
       <span class="title-text">累積參與次數</span>
     </div>
-    <p class="goal-text">目標 {{ currentGoal }} 次</p>
+    <p class="goal-text">
+      <span v-if="loading" class="text-skeleton text-skeleton--goal" />
+      <template v-else>目標 {{ currentGoal }} 次</template>
+    </p>
     <div class="bar-track">
-      <div class="bar-fill" :style="{ width: progressPercent + '%' }" />
+      <div v-if="loading" class="bar-skeleton" />
+      <div v-else class="bar-fill" :style="{ width: progressPercent + '%' }" />
     </div>
     <div class="count-num">
       <span v-if="loading" class="count-skeleton" />
@@ -127,11 +131,38 @@ const iconBurn = import.meta.env.BASE_URL + 'images/icon-burn.png'
   letter-spacing: -2px;
 }
 
-.count-skeleton {
+@keyframes shimmer {
+  0% { background-position: -200% center; }
+  100% { background-position: 200% center; }
+}
+
+.count-skeleton,
+.text-skeleton,
+.bar-skeleton {
   display: inline-block;
-  width: 48px;
-  height: 56px;
   border-radius: 8px;
-  background: #e6e8f0;
+  background: linear-gradient(90deg, #e6e8f0 25%, #f0f2f7 50%, #e6e8f0 75%);
+  background-size: 200% 100%;
+  animation: shimmer 1.4s ease infinite;
+}
+
+.count-skeleton {
+  width: 72px;
+  height: 64px;
+  border-radius: 10px;
+}
+
+.text-skeleton--goal {
+  width: 72px;
+  height: 16px;
+  border-radius: 6px;
+  vertical-align: middle;
+}
+
+.bar-skeleton {
+  display: block;
+  width: 100%;
+  height: 100%;
+  border-radius: 25px;
 }
 </style>
